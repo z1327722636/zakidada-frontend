7c88c55558abfee99b7f91e7b272b37d43be1e84
z1327722636 <1327722636@qq.com>
2024-08-14 15:53:06 +0800

    ts类型校验不兼容

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './routes'
-import {useUnsavedChangesStore} from "@/store/unsavedChangesStore";
+import { useUnsavedChangesStore } from '@/store/unsavedChangesStore'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
 router.beforeEach((to, from, next) => {
-  const unsavedChangesStore = useUnsavedChangesStore();
+  const unsavedChangesStore = useUnsavedChangesStore()
   if (unsavedChangesStore.checkUnsavedChanges()) {
-    unsavedChangesStore.setUnsavedChanges(false); // 清除未保存的标记
-    next();
+    unsavedChangesStore.setUnsavedChanges(false) // 清除未保存的标记
+    next()
   } else {
-    next(false);
+    next(false)
   }
-});
-
+})
 
 export { router }
